77daf58c3903e81567b58dfae985b1e18566b85b
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-12-02 10:24:38 +0530

    Form

--- react_newcadbury/src/App.js
+++ react_newcadbury/src/App.js
@@ -1,9 +1,9 @@
-import Table from './Table'
+import Form from './Form'
 
 let App = () => {
 return <div>
   <h1></h1>
-  <Table/>
+  <Form/>
 </div>
 
 }
---
+++ react_newcadbury/src/Form.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Component } from "react";
+
+class Form extends Component{
+  state = {
+    User_name:"",
+    Email:"",
+    Phon_NO: "",
+    Password:""
+  }
+
+   updateHandler = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+   }
+   submitHandler(event){
+       event.preventDefault();
+       
+   }
+    render(){
+        return <div>
+            <h1>Form Validation</h1><br/><br/>
+            <pre>{JSON.stringify(this.state)}</pre>
+            <form>
+                <label>User Name:</label>
+            <input type="text" onChange={this.updateHandler} name = "User_name"/><br/>
+            <label>Email:</label>
+            <input type="text" onChange={this.updateHandler} name = "Email" /><br/>
+            <label>Phone NO:</label>
+            <input type="text" onChange={this.updateHandler} name = "Phon_NO" /><br/>
+            <label>Password:</label>
+            <input type="text" onChange={this.updateHandler} name = "Password" /><br/>
+            
+            <input type="submit" onChange={this.submitHandler}/>
+            </form>
+        </div>
+    }
+}
+export default Form
